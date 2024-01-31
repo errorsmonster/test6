60a9e54a98bedef17b1da18d9d745cc34e30a5ae
Raazarya12 <125205649+Raazarya12@users.noreply.github.com>
2024-02-01 01:48:48 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -863,7 +863,7 @@ async def send_all(bot, userid, files, ident):
                             InlineKeyboardButton("Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ", url=GRP_LNK),
                             InlineKeyboardButton("Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ", url=CHNL_LNK),
                         ],
-                        [InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/J_shree_ram")],
+                        [InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Titanoboa_team")],
                     ]
                 ),
             )
@@ -970,7 +970,7 @@ async def send_all(bot, userid, files, ident, chat_id, user_name, query):
                                 InlineKeyboardButton("Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ", url=GRP_LNK),
                                 InlineKeyboardButton("Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ", url=CHNL_LNK),
                             ],
-                            [InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/J_shree_ram")],
+                            [InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Titanoboa_team")],
                         ]
                     ),
                 )
@@ -1012,7 +1012,7 @@ async def send_all(bot, userid, files, ident, chat_id, user_name, query):
                         InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                         InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                     ],[
-                        InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/J_shree_ram")
+                        InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Titanoboa_team")
                         ]
                     ]
                 )
